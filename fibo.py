e5d675143e3219f117ecba40ba93e74f73bdf20c
Kimtaeheon95 <70333648+Kimtaeheon95@users.noreply.github.com>
2020-11-10 19:51:39 +0900

    계산기

---
+++ fibo.py
@@ -0,0 +1,20 @@
+def plus():
+    a = input("첫번째 숫자 입력:")
+    b = input("두번째 숫자 입력:")
+    print(int(a) + int(b))
+
+def minus():
+    a = input("첫번째 숫자 입력:")
+    b = input("두번째 숫자 입력:")
+    print(int(a) - int(b))
+
+def multiply():
+    a = input("첫번째 숫자 입력:")
+    b = input("두번째 숫자 입력:")
+    print(int(a) * int(b))
+
+def division(a,b):
+    a = input("첫번째 숫자 입력:")
+    b = input("두번째 숫자 입력:")
+    print(int(a) / int(b)
+print(1)
